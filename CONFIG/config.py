b34dc779b93b1f683548be29c1898ab3811dd4c9
Artemii <cvindergocvin@gmail.com>
2024-12-06 22:20:34 +0300

    start

---
+++ CONFIG/config.py
@@ -0,0 +1,22 @@
+class DataCallBack:
+    GLOBAL_DCITER = {
+        'Профиль': '1',
+        'Пройти опрос': '1.1',
+        'Заказать опрос': '1.2',
+        }
+    
+    ADMIN_FUNC = {
+        'Добавить Тему': 'admin.1'
+    }
+
+
+class DbCondig:
+    DBNAME = 'PULL'
+    USER = 'postgres'
+    PASSWORD = '$$'
+    HOST = 'localhost'
+    PORT = '5432'
+
+
+class TonWallet:
+    ADDRESS = "UQD-GWFvMpW8px3ETDOdejZJKaesWlT3raYck0SGFVap5sTe"
